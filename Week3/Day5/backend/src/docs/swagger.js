dd07d880105b5a56d641a9c8e89541512195ec96
[ShahamHijab] <shaham434789@gmail.com>
2025-08-18 15:12:47 +0500

    changed url in swagger,js

--- Week3/Day5/backend/src/docs/swagger.js
+++ Week3/Day5/backend/src/docs/swagger.js
@@ -15,7 +15,7 @@ const options = {
     },
     servers: [
       {
-        url: process.env.BASE_URL || "http://localhost:5000",
+        url: "shahamweek3day5backend.vercel.app",
         description: "Development server",
       },
     ],
